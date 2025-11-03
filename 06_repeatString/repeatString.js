40dbb8e9b8ce998d511cb216394e1c1e16f3037d
Neil Benolirao <neilskillsets.initiative@gmail.com>
2025-11-03 11:59:57 +0800

    fix: use if statemnent, for loop to repeat string given n times

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function (word, num) {
+  let string = "";
 
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < num; i++) {
+    string += word;
+  }
+
+  return string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
+
+// Takes a string
+// Repeat string based on 2nd arguments num
+// Use loops
+//
